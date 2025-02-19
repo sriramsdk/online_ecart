88befa6c73f41cdbe5fdc7482e8a240e9b0af436
Sriram.R <146877320+sriramsdk@users.noreply.github.com>
2025-02-19 21:46:53 +0530

    saving of products

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // Admin side controller
 class Admin extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Product_Model');
+    }
+
     // index function
     public function index(){
 
@@ -55,7 +60,53 @@ class Admin extends CI_Controller {
     }
 
     public function add_product(){
-        echo "<pre>";print_r($_POST);exit();
+
+        $upload_path = "./uploads/products/";
+
+        if (!is_dir($upload_path)) {
+            mkdir($upload_path, 0777, true);
+        }
+
+        $config['upload_path'] = $upload_path;
+        $config['allowed_types'] = "jpg|jpeg|png|gif";
+        $config['max_size'] = 10000;
+
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('product_image')) {
+            $error = $this->upload->display_errors();
+            echo json_encode(['status' => 'failed', 'message' => 'Failed to upload image']);
+        } else {
+            $upload_data = $this->upload->data();
+            $image_path = 'uploads/products/' . $upload_data['file_name'];
+
+            $product_name = $this->input->post('product_name');
+            $product_price = $this->input->post('product_price');
+            $product_discount = $this->input->post('product_discount');
+            $final_price = $this->input->post('product_final_price');
+            $company = $this->input->post('company');
+            $category = $this->input->post('category');
+
+            $product_data = array(
+                'product_name' => $product_name,
+                'product_price' => $product_price,
+                'discount' => $product_discount,
+                'product_discount' => $final_price,
+                'product_image' => $image_path,
+                'company' => $company,
+                'category' => $image_path
+            );
+
+            $response = $this->Product_Model->insert_product($product_data);
+
+            if($response){
+                echo json_encode(['status' => 'success', 'message' => "Product added successfully"]);
+            }else{
+                echo json_encode(['status' => 'failed', 'message' => "Product not added"]);   
+            }
+            
+        }
     }
 
 }
---
+++ application/models/Product_Model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product_Model extends CI_Model{
+
+    public function insert_product($product_data) {
+        $this->db->insert('products', $product_data);  // Assuming the table is named 'products'
+        return $this->db->insert_id(); // Return the insert ID to confirm the record was added
+    }
+
+}
+
+?>
